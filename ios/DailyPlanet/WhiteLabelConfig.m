9e410fdb42a4746a13dc9b9deffefd915a95ac77
Davidson Marra <davidsonmarra@CPU-00335.local>
2023-03-10 11:39:09 -0300

    :building_construction: add new schemes on ios
    add DailyBugle target and DailyPlanet target on ios

---
+++ ios/DailyPlanet/WhiteLabelConfig.m
@@ -0,0 +1,19 @@
+//
+//  WhiteLabelConfig.m
+//  DailyPlanet
+//
+//  Created by Davidson Marra on 10/03/23.
+//
+
+#import <Foundation/Foundation.h>
+#import "WhiteLabelConfig.h"
+
+@implementation WhiteLabelConfig
+
+  RCT_EXPORT_MODULE(WhiteLabelConfig);
+ 
+  RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(getAppName) {
+    return @"Daily Planet";
+  }
+
+@end
---
+++ ios/WhiteLabelConfig.h
@@ -0,0 +1,10 @@
+//
+//  WhiteLabelConfig.h
+//  whiteLabelNews
+//
+//  Created by Davidson Marra on 10/03/23.
+//
+
+#import <React/RCTBridgeModule.h>
+@interface WhiteLabelConfig : NSObject<RCTBridgeModule>
+@end
